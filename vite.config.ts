23ed69b41b2404fb0d0b06b000b6d2cf134ad545
denikeev <script.edf@gmail.com>
2023-09-22 01:55:03 +0300

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
+import { dirname } from 'path';
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const currentDir = __dirname.split('/').slice(-1).join();
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +19,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: `${currentDir}/assets/[name]-[hash][extname]`,
+        chunkFileNames: `${currentDir}/assets/[name].js`,
+        entryFileNames: `${currentDir}/assets/[name]-[hash].js`
+      }
+    },
+  },
 })
